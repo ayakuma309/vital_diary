668f90b16e41c7536eb4126b8e3be146db5a1567
ayakuma309 <ayako.30901@gmail.com>
2022-12-10 16:08:30 +0900

    add: ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #user新規登録
   resources :users, only: %i[new create]
+
+  #ログイン
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
