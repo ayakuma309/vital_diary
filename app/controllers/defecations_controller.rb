8b1b58c39fa12e2e8c65209898622fef59529ade
ayakuma309 <ayako.30901@gmail.com>
2022-12-12 15:39:39 +0900

    add: defecationを非同期化

--- app/controllers/defecations_controller.rb
+++ app/controllers/defecations_controller.rb
@@ -1,14 +1,12 @@
 class DefecationsController < ApplicationController
 
   def create
-    vital = Vital.find(params[:vital_id])
-    current_user.defecation(vital)
-    redirect_back fallback_location: vitals_path, success: t('.success')
+    @vital = Vital.find(params[:vital_id])
+    current_user.defecation(@vital)
   end
 
   def destroy
-    vital = Vital.find(params[:vital_id])
-    defecation = current_user.undefecation(vital)
-    redirect_back fallback_location: vitals_path, success: t('.success')
+    @vital = Vital.find(params[:vital_id])
+    defecation = current_user.undefecation(@vital)
   end
 end
