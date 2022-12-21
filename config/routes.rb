964ee35cd9d9a918122a82b86c9aa87b5a04a512
ayakuma309 <ayako.30901@gmail.com>
2022-12-21 15:28:17 +0900

    add: 利用規約、プライバシーポリシーの追加 #23

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #topページ
   root 'static_pages#top'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
+
   #user新規登録
   resources :users, only: %i[new create]
 
