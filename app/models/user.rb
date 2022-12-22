cc87924c81ac181c6ffe870f6131942ff458b091
ayakuma309 <ayako.30901@gmail.com>
2022-12-22 20:14:24 +0900

    add: profileモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,8 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  has_one :profile, dependent: :destroy
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
