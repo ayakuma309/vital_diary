570ddbef17c7d1bc942956ca3aa393aaeb032f5c
ayakuma309 <ayako.30901@gmail.com>
2022-12-23 12:56:44 +0900

    add: userと既往歴の関連づけとvalidation追加

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -15,4 +15,11 @@
 #  index_medical_histories_on_user_id  (user_id)
 #
 class MedicalHistory < ApplicationRecord
+  belongs_to :user
+
+  validates :disease_name , presence: true
+  validates :when_onset , presence: true
+  validates :in_treatment, inclusion: {in: [true, false]}
+
+  scope :is_during_treatment, -> (value){ where(in_treatment: value).order(when_onset: :asc) }
 end
